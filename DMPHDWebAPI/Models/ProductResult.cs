a0b8264f9768dceb82001d91816c0f1a33bbdc6f
ghostwood1071 <thinhquangshin@gmail.com>
2021-07-13 12:47:38 +0700

    change models

---
+++ DMPHDWebAPI/Models/ProductResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMPHDWebAPI.Models
+{
+    public class ProductResult
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public double BasePrice { get; set; }
+
+    }
+}
